2764fab729c414dc320774d3f0308dad806bed5c
Louis Sullivan <sullivanlouis0@gmail.com>
2022-09-20 23:24:24 +0100

    added message

--- job-script/convert-supplier.sh
+++ job-script/convert-supplier.sh
@@ -3,3 +3,6 @@
 rm -r final.xlsx supplier_product_list.xlsx > /dev/null 2>&1
 curl --output supplier_product_list.xlsx -X GET https://raw.githubusercontent.com/louissullivan4/supplier-file-converter-devcon-demo/server/master/supplier_product_list.xlsx
 curl --output final.xlsx -X POST http://sullivls.pythonanywhere.com/upload -H "Content-Type: multipart/form-data" -F "file=@supplier_product_list.xlsx"
+
+ls -l
+echo "Process was complete"
